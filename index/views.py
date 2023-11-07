818635795766f159001eae5aa970307ee6ab8564
nuttanonyolnawa <145351490+nuttanonyolnawa@users.noreply.github.com>
2023-11-07 16:18:18 +0700

    Add files via upload

--- index/views.py
+++ index/views.py
@@ -19,9 +19,74 @@ def index(request) :
     return render(request, 'index/index.html', data)
 
 # class PaymentList(View):
+class PaymentList(View):
+    def get(self, request):
+        payments = list(Payment.objects.all().values())
+        data = dict()
+        data['payments'] = payments
 
+        return JsonResponse(data)
 
 # class ProductList(View) :
+class ProductList(View):
+    def get(self, request):
+        products = list(Product.objects.all().values())
+        data = dict()
+        data['products'] = products
 
+        return JsonResponse(data)
 
 # class ProductDetail(View);
+class ProductDetail(View):
+    def get(self, request, pk, pk2):
+
+        product_type = pk + '/' + pk2
+
+        product = list(Product.objects.select_related('customer_code')
+            .filter(product_type=product_type)
+            .values('product_code', 'product_name', 'animal_type', 'product_price', 'product_type'))
+        ProductType = list(ProductType.objects.select_related('product_code')
+            .filter(product_type=product_type)
+            .values('product_type', 'product_description'))
+
+        data = dict()
+        data['product'] = product
+        data['producttype'] = ProductType
+
+        return JsonResponse(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ProductDetail(View):
+    def get(self, request, pk, pk2):
+
+        product_code = pk + '/' + pk2
+
+        Product = list(Product.objects.select_related('animal_type')
+            .filter(product_type=product_type)
+            .values('product_code', 'product_name', 'animal_type', 'product_price', 'product_type'))
+        Producttype = list(productLineItem.objects.select_related('product_code')
+            .filter(product_code=product_code)
+            .values('invoice_no', 'item_no', 'product_code', 'product_code__name', 'unit_price'
+            , 'quantity', 'product_total'))
+
+        data = dict()
+        data['product'] = invoice
+        data['productlineitem'] = productlineitem
+
+        return JsonResponse(data)
