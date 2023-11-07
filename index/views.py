312a4583aae248f73defe18608106df107c8c6a1
0Phonlakit <119070396+0Phonlakit@users.noreply.github.com>
2023-11-07 15:13:05 +0700

    modified views.py
    modified views.py

--- index/views.py
+++ index/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views.generic import View
+from django.http import JsonResponse
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.forms.models import model_to_dict
+from django.db.models import Max
+from django.db import transaction
+from .models import *
+import json
+import re
 
 # Create your views here.
+def index(request) :
+    data = {}
+    return render(request, 'index/index.html', data)
+
+# class PaymentList(View):
+
+
+# class ProductList(View) :
+
+
+# class ProductDetail(View);
